30371473040b5eb6774caa237f0d7c1ab9df0507
Sthephanie-Leon-Vargas <sleonv@ucenfotec.ac.cr>
2025-06-12 22:18:33 -0600

    Se crean los Entity de producto y categoria

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# java-spring
-Proyecto spring base para cursos de proyecto
+# java-spring-Tarea1
+Proyecto spring Boot Tarea 1 Curso de Proyecto 3 Universidad Cenfotec
---
+++ src/main/java/com/project/demo/logic/entity/producto/Producto.java
@@ -0,0 +1,31 @@
+package com.project.demo.logic.entity.producto;
+
+import com.project.demo.logic.entity.categoria.Categoria;
+import jakarta.persistence.*;
+
+@Table(name = "producto")
+@Entity
+public class Producto {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+private String nombre;
+private String descripcion;
+private double precio;
+private int cantidadStock;
+@ManyToOne
+@JoinColumn(name = "categoria_id")
+private Categoria categoria;
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Producto() {
+    }
+
+}
